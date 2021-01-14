717ee0e9092aa10286668de5794cae23e0bd5037
Artem Kulin <kulinartem@gmail.com>
2021-01-15 01:36:03 +0300

    rework UserDaoImp.class: add realization for method getUserByCar

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,12 +1,15 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserDaoImp implements UserDao {
@@ -22,8 +25,22 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("FROM User");
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCar(String model, int series) {
+      String carId = "FROM Car WHERE "
+              + "model = :parModel"
+              + " AND SERIES = :parSeries";
+      Query subQuery = sessionFactory.getCurrentSession().createQuery(carId);
+      subQuery.setParameter("parModel", model);
+      subQuery.setParameter("parSeries", series);
+      Car car = (Car) subQuery.setMaxResults(1).getSingleResult();
+      long id = car.getId();
+      Query query = sessionFactory.getCurrentSession().createQuery("FROM User WHERE car_id = :car_id");
+      query.setParameter("car_id", id);
+      return (User) query.getSingleResult();
+   }
 }
